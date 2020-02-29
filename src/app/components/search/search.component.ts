073ee38dccf08cb32e3fed7f963abc83883447c8
Chara- <escor05_11@hotmail.com>
2020-02-29 00:51:22 -0500

    adding search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent  {
 
-  constructor() { }
+  artists: any[] = [];
 
-  ngOnInit(): void {
-  }
+  constructor( private spotify: SpotifyService ) { }
+
+ search( term: string ){
+    console.log( term );
+    this.spotify.getArtist( term ).subscribe( (data : any) => {
+      console.log(data.artists.items);
+      this.artists = data.artists.items;
+    });
+ }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,11 +9,24 @@ export class SpotifyService {
   getNewReleases() {
     const headers = new HttpHeaders({
       Authorization:
-        "Bearer BQBiIOt6MqWMzp1IHcvPxwZbLvBZPljOX_YvZqItauN8tF2SQYMZuB80xmpMbLOMgl6Usleru72FWvEgzog"
+        "Bearer BQD61WMsLhNgZZg3iODy26rq45U2xyNy19MlaxaFKrR1CH_izuut3mxRByrm6CgNChATwRn8XvVKCX5Abtk"
     });
     return this.http.get(
       "https://api.spotify.com/v1/browse/new-releases?limit=20",
       { headers }
     );
+  }
+
+  getArtist( term: string ) {
+
+    const headers = new HttpHeaders({
+      Authorization:
+        "Bearer BQD61WMsLhNgZZg3iODy26rq45U2xyNy19MlaxaFKrR1CH_izuut3mxRByrm6CgNChATwRn8XvVKCX5Abtk"
+    });
+    return this.http.get(
+      `https://api.spotify.com/v1/search?q=${ term }&type=artist&limit=15`,
+      { headers }
+    );
+
   }
 }
