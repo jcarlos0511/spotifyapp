c08353fb2332d69ada29ff42daa015e4dad3f1af
Chara- <escor05_11@hotmail.com>
2020-03-01 02:53:53 -0500

    bug in loading fixed

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { SpotifyService } from 'src/app/services/spotify.service';
+import { Component, OnInit } from "@angular/core";
+import { SpotifyService } from "src/app/services/spotify.service";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
+  selector: "app-search",
+  templateUrl: "./search.component.html",
   styles: []
 })
-export class SearchComponent  {
-
+export class SearchComponent {
   artists: any[] = [];
+  loading: boolean;
 
-  constructor( private spotify: SpotifyService ) { }
+  constructor(private spotify: SpotifyService) {}
 
- search( term: string ){
-    console.log( term );
-    this.spotify.getArtist( term ).subscribe( (data : any) => {
+  search(term: string) {
+    this.loading = true;
+    console.log(term);
+    this.spotify.getArtist(term).subscribe((data: any) => {
       console.log(data);
       this.artists = data;
+      this.loading = false;
     });
- }
-
+  }
 }
